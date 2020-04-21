e2ba19e022e36daae4b9474daef733400f779508
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-21 01:38:48 -0700

    added all the interface methods from Layer

--- layers/application.go
+++ layers/application.go
@@ -12,3 +12,18 @@ type ApplicationLayer struct {
 func NewApplicationLayer(up, down *Layer) *ApplicationLayer {
 	return &ApplicationLayer{"Application", up, down}
 }
+
+// GetName returns the name of the layer
+func (ap ApplicationLayer) GetName() string {
+	return ap.name
+}
+
+// SendData is a function that the layer uses to move data up or down
+func (ap *ApplicationLayer) SendData(to *Layer, data interface{}) bool {
+	return true
+}
+
+// ReceiveData is a function that the layer uses to get data from other layers
+func (ap *ApplicationLayer) ReceiveData(from *Layer, data interface{}) bool {
+	return true
+}
