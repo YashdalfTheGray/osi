97a86435fca089d0b7e36e8978f944d8f77e14b9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-21 01:44:41 -0700

    added a layers enum to the project

--- layers/layer.go
+++ layers/layer.go
@@ -6,3 +6,17 @@ type Layer interface {
 	SendData(to *Layer, data interface{}) bool
 	ReceiveData(from *Layer, data interface{}) bool
 }
+
+// LayerName is used to represent a layer
+type LayerName int
+
+// This "enum" is here to make the layer names into constants
+const (
+	Physical LayerName = iota + 1
+	DataLink
+	Network
+	Transport
+	Session
+	Presentation
+	Application
+)
