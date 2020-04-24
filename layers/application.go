3a90ec755d2b7e317becb2bba69db4c88bbf9216
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-23 20:25:26 -0700

    make the name field private, use the name method

--- layers/application.go
+++ layers/application.go
@@ -3,7 +3,7 @@ package layers
 // ApplicationLayer represents the highest layer of the OSI stack
 // It accepts data directly from the user
 type ApplicationLayer struct {
-	Name LayerName
+	name LayerName
 	up   *Layer
 	down *Layer
 }
@@ -13,12 +13,17 @@ func NewApplicationLayer(up, down *Layer) *ApplicationLayer {
 	return &ApplicationLayer{Application, up, down}
 }
 
+// Name returns the name of the layer
+func (al ApplicationLayer) Name() LayerName {
+	return al.name
+}
+
 // SendData is a function that the layer uses to move data up or down
-func (ap *ApplicationLayer) SendData(to *Layer, data interface{}) bool {
+func (al *ApplicationLayer) SendData(to *Layer, data interface{}) bool {
 	return true
 }
 
 // ReceiveData is a function that the layer uses to get data from other layers
-func (ap *ApplicationLayer) ReceiveData(from *Layer, data interface{}) bool {
+func (al *ApplicationLayer) ReceiveData(from *Layer, data interface{}) bool {
 	return true
 }
