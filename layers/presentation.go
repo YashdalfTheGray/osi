868e849901d5cf2537d41c26ab517147ebf927a5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-23 20:25:56 -0700

    equalize the receiver name, create an exported name method

--- layers/presentation.go
+++ layers/presentation.go
@@ -3,7 +3,7 @@ package layers
 // PresentationLayer represents the highest layer of the OSI stack
 // It accepts data directly from the user
 type PresentationLayer struct {
-	Name LayerName
+	name LayerName
 	up   *Layer
 	down *Layer
 }
@@ -13,12 +13,17 @@ func NewPresentationLayer(up, down *Layer) *PresentationLayer {
 	return &PresentationLayer{Presentation, up, down}
 }
 
+// Name returns the name of the layer
+func (pl PresentationLayer) Name() LayerName {
+	return pl.name
+}
+
 // SendData is a function that the layer uses to move data up or down
-func (ap *PresentationLayer) SendData(to *Layer, data interface{}) bool {
+func (pl *PresentationLayer) SendData(to *Layer, data interface{}) bool {
 	return true
 }
 
 // ReceiveData is a function that the layer uses to get data from other layers
-func (ap *PresentationLayer) ReceiveData(from *Layer, data interface{}) bool {
+func (pl *PresentationLayer) ReceiveData(from *Layer, data interface{}) bool {
 	return true
 }
