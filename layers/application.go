050781aa1323627b97a6aa1c75fec1878130d760
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-25 00:00:34 -0700

    dropped all the pointers, just use the layer interface

--- layers/application.go
+++ layers/application.go
@@ -6,13 +6,13 @@ import "fmt"
 // It accepts data directly from the user
 type ApplicationLayer struct {
 	name LayerName
-	up   *Layer
-	down *Layer
+	up   Layer
+	down Layer
 }
 
 // NewApplicationLayer creates a new instance of ApplicationLayer
-func NewApplicationLayer(up, down *Layer) *ApplicationLayer {
-	return &ApplicationLayer{Application, up, down}
+func NewApplicationLayer(up, down Layer) ApplicationLayer {
+	return ApplicationLayer{Application, up, down}
 }
 
 // Name returns the name of the layer
@@ -21,28 +21,27 @@ func (al ApplicationLayer) Name() LayerName {
 }
 
 // SendData is a function that the layer uses to move data up or down
-func (al *ApplicationLayer) SendData(to *Layer, data interface{}) bool {
+func (al ApplicationLayer) SendData(to Layer, data interface{}) bool {
 	return true
 }
 
 // ReceiveData is a function that the layer uses to get data from other layers
-func (al *ApplicationLayer) ReceiveData(from *Layer, data interface{}) bool {
-	if (*from).Name() == Presentation {
+func (al ApplicationLayer) ReceiveData(from Layer, data interface{}) bool {
+	if from.Name() == Presentation {
 		return al.receiveDataFromPresentation(data)
 	}
 	return al.receiveDataFromUser(data)
 }
 
-func (al *ApplicationLayer) receiveDataFromUser(data interface{}) bool {
+func (al ApplicationLayer) receiveDataFromUser(data interface{}) bool {
 	return al.SendData(al.down, data)
 }
 
-func (al *ApplicationLayer) receiveDataFromPresentation(data interface{}) bool {
+func (al ApplicationLayer) receiveDataFromPresentation(data interface{}) bool {
 	if str, ok := data.(string); ok {
 		fmt.Println(str)
 		return true
-	} else {
-		fmt.Println("Got malformed data from the presentation layer")
-		return false
 	}
+	fmt.Println("Got malformed data from the presentation layer")
+	return false
 }
