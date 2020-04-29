d89c82966433492cb4f6bd0c77fb1d4a094bf194
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-29 01:32:49 -0700

    changed up the session layer to use the models

--- layers/session.go
+++ layers/session.go
@@ -1,31 +1,33 @@
 package layers
 
+import "github.com/YashdalfTheGray/osi/models"
+
 // SessionLayer represents the highest layer of the OSI stack
 // It accepts data directly from the user
 type SessionLayer struct {
-	name LayerName
-	up   Layer
-	down Layer
+	name models.LayerName
+	up   models.Layer
+	down models.Layer
 }
 
 // NewSessionLayer creates a new instance of SessionLayer
-func NewSessionLayer(up, down Layer) *SessionLayer {
-	return &SessionLayer{Presentation, up, down}
+func NewSessionLayer(up, down models.Layer) *SessionLayer {
+	return &SessionLayer{models.Presentation, up, down}
 }
 
 // Name returns the name of the layer
-func (sl SessionLayer) Name() LayerName {
+func (sl SessionLayer) Name() models.LayerName {
 	return sl.name
 }
 
 // SendData is a function that the layer uses to move data up or down
-func (sl SessionLayer) SendData(to Layer, data interface{}) bool {
+func (sl SessionLayer) SendData(to models.Layer, data interface{}) bool {
 	return to.ReceiveData(sl, data)
 }
 
 // ReceiveData is a function that the layer uses to get data from other layers
-func (sl SessionLayer) ReceiveData(from Layer, data interface{}) bool {
-	if from.Name() == Presentation {
+func (sl SessionLayer) ReceiveData(from models.Layer, data interface{}) bool {
+	if from.Name() == models.Presentation {
 		return sl.receiveDataFromPresentation(data)
 	}
 	return sl.receiveDataFromTransport(data)
