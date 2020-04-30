52e5b02d2ac6714fda8959f5b948bcda358b5b1a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-04-30 00:14:48 -0700

    changed struct members to exported and added a string method

--- models/message.go
+++ models/message.go
@@ -1,14 +1,20 @@
 package models
 
+import "fmt"
+
 // Message contains some data that needs to be sent to a
 // location on the network
 type Message struct {
-	data    interface{}
-	address NetworkLocation
+	Data    interface{}
+	Address NetworkLocation
 }
 
 // NewMessage constructs a new instance of Message
 func NewMessage(data interface{}, address string) Message {
 	loc := NewNetworkLocation(address)
-	return Message{data: data, address: loc}
+	return Message{Data: data, Address: loc}
+}
+
+func (m Message) String() string {
+	return fmt.Sprintf("Message to %s\n with content\n%s", m.Address.rawAddress, m.Data)
 }
